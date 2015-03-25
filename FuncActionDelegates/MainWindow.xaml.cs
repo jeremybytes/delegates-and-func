6b300e2d7b4f737bac7677c4651d071438d7abe2
Jeremy Clark <jeremy@jeremybytes.com>
2015-03-25 09:10:29 -0700

    Added multi-cast Action code

--- FuncActionDelegates/MainWindow.xaml.cs
+++ FuncActionDelegates/MainWindow.xaml.cs
@@ -10,6 +10,9 @@ namespace FuncActionDelegates
         //PersonFormat formatPerson;
         Func<Person, string> formatPerson;
 
+        //public delegate void PeopleAction(List<Person> input);
+        Action<List<Person>> actOnPeople;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,12 +31,44 @@ namespace FuncActionDelegates
                 formatPerson = p => string.Format("{0}, {1}", p.LastName, p.FirstName);
         }
 
+        public void AssignAction()
+        {
+            actOnPeople = null;
+            if (OptionCButton.IsChecked.Value)
+                actOnPeople += p => MessageBox.Show(
+                    p.OrderByDescending(r => r.Rating).First().LastName);
+
+            if (OptionAButton.IsChecked.Value)
+                actOnPeople += p => OutputList.Items.Add(
+                    p.Average(r => r.Rating).ToString("#.#"));
+
+            if (OptionBButton.IsChecked.Value)
+                actOnPeople += p => OutputList.Items.Add(p.Min(s => s.StartDate));
+
+            if (OptionDButton.IsChecked.Value)
+                actOnPeople += p =>
+                {
+                    p.ForEach(c => Console.Write(c.LastName[0]));
+                    Console.WriteLine();
+                };
+        }
+
         private void ProcessDataButton_Click(object sender, RoutedEventArgs e)
         {
             OutputList.Items.Clear();
-            AssignDelegate();
-            foreach (Person person in PersonListBox.Items)
-                OutputList.Items.Add(person.ToString(formatPerson));
+            if (StringExpander.IsExpanded)
+            {
+                AssignDelegate();
+                foreach (Person person in PersonListBox.Items)
+                    OutputList.Items.Add(person.ToString(formatPerson));
+            }
+            if (ActionExpander.IsExpanded)
+            {
+                AssignAction();
+                var people = People.GetPeople();
+                if (actOnPeople != null)
+                    actOnPeople(people);
+            }
         }
     }
 }
